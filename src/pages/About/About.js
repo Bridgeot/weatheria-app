d624da96a9e93951385181370ba1d4613b90e1b5
Bridgeot <bridgeot789@gmail.com>
2023-05-17 17:52:59 -0400

    add about content

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,20 @@
-import "./About.css";
+import './About.css';
 
 function About(props) {
     return (
         <section className='about-section'>
-            <h1>About Page Component</h1>
+            <div className='about-container'>
+                <div className='about-align'>
+                    <h1 className='about-title'>About Weatheria</h1>
+                    <div className='about-statement'>
+                        <p>
+                            Want to know what the weather is going to be like today? 
+                            Just open the Weatheria app to get a quick forecast of your local area. 
+                            You can also search weather conditions in cities to see what the weather is like there and get a heads-up before you make plans to travel.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
