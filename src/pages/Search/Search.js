6892aae8834d1f9f7afd83341c26c27a26ffd134
Bridgeot <bridgeot789@gmail.com>
2023-05-17 10:41:30 -0400

    add additional weather conditions data and remove extraneous code

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -4,13 +4,7 @@ import axios from 'axios';
 import './Search.css';
 
 function Search(props) {
-    const [ condition, setCondition ] = useState({
-        name: 'London',
-        description: 'clear',
-        temp: 80,
-        maxTemp: 91,
-        minTemp: 70,
-    }); 
+    const [ condition, setCondition ] = useState({});
 
     useEffect(() => {
         const url = 'https://api.openweathermap.org/data/2.5/weather?q=North+Lauderdale&appid=fa0c49e26635d4780fb49a638b2ada3c&units=imperial';
@@ -23,6 +17,14 @@ function Search(props) {
             temp: Math.round(response.data.main.temp), 
             maxTemp: Math.round(response.data.main.temp_max), 
             minTemp: Math.round(response.data.main.temp_min), 
+            feelsLike: Math.round(response.data.main.feels_like), 
+            humidity: response.data.main.humidity, 
+            pressure: response.data.main.pressure, 
+            wind: Math.round(response.data.wind.speed), 
+            clouds: response.data.clouds.all, 
+            visibility: Math.floor(response.data.visibility / 1000), 
+            sunrise: response.data.sys.sunrise, 
+            sunset: response.data.sys.sunset,
             })
           })
           .catch((error) => {
@@ -41,7 +43,7 @@ function Search(props) {
             <div className='weather'>
                 <div className='weather-main'>
                     <h1 className='weather-city'>{condition.name}</h1>
-                    <h2 className='weather-degree'>{condition.temp}&deg;</h2>
+                    <h2 className='weather-number'>{condition.temp}&deg;</h2>
                     <h3>{condition.description}</h3>
                     <div className='weather-sub'>
                         <p>H: {condition.maxTemp}&deg;</p>
@@ -52,29 +54,37 @@ function Search(props) {
             <div className='weather-conditions1'>
                 <div className='condition'>
                     <h2 className='condition-name'>Feels Like</h2>
+                    <p className='condition-number'>{condition.feelsLike}&deg;</p>
                 </div>
                 <div className='condition'>
                     <h2 className='condition-name'>Humidity</h2>
+                    <p className='condition-number'>{condition.humidity}&#37;</p>
                 </div>
                 <div className='condition'>
                     <h2 className='condition-name'>Wind</h2>
+                    <p className='condition-number'>{condition.wind} mph</p>
                 </div>
                 <div className='condition'>
                     <h2 className='condition-name'>Pressure</h2>
+                    <p className='condition-number'>{condition.pressure}</p>
                 </div>
             </div>
             <div className='weather-conditions2'>
                 <div className='condition'>
                     <h2 className='condition-name'>Visibility</h2>
+                    <p className='condition-number'>{condition.visibility} km</p>
                 </div>
                 <div className='condition'>
                     <h2 className='condition-name'>Sunrise</h2>
+                    <p className='condition-number'>{condition.sunrise}</p>
                 </div>
                 <div className='condition'>
                     <h2 className='condition-name'>Sunset</h2>
+                    <p className='condition-number'>{condition.sunset}</p>
                 </div>
                 <div className='condition'>
                     <h2 className='condition-name'>Clouds</h2>
+                    <p className='condition-number'>{condition.clouds}&#37;</p>
                 </div>
             </div>
         </section>
