3e10b43abc6590c34dd7c5a85bbb7940f6178b57
Bridgeot <bridgeot789@gmail.com>
2023-05-17 17:52:06 -0400

    change link name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ function Header(props) {
                 <img id='logo-img' src={logo} alt='weatheria logo' />
             </Link>
             <div className='links-align'>
-                <Link to='/'>About</Link>
+                <Link to='/'>Home</Link>
                 <Link to='/geolocation'>My Location</Link>
                 <Link to='/search'>Search</Link>
                 <Link to='/viewhistory'>View History</Link>
