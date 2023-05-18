aa53c465d8ec08e6ea22e468db46b5e99b6159c0
Bridgeot <bridgeot789@gmail.com>
2023-05-18 18:28:46 -0400

    update renamed file

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,22 @@
+import './Home.css';
+
+function Home() {
+    return (
+        <section className='about-section'>
+            <div className='about-container'>
+                <div className='about-align'>
+                    <h1 className='about-title'>About Weatheria</h1>
+                    <div className='about-statement'>
+                        <p>
+                            Want to know what the weather is going to be like today? 
+                            Just open the Weatheria app to get a quick forecast of your local area. 
+                            You can also search weather conditions in cities to see what the weather is like there and get a heads-up before you make plans to travel.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Home;
