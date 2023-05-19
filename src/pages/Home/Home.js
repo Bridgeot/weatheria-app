9287f5e186738edfa4306629a5ea8f1bc0da94fe
Bridgeot <bridgeot789@gmail.com>
2023-05-19 01:37:35 -0400

    update about statement

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,9 +8,9 @@ function Home() {
                     <h1 className='about-title'>About Weatheria</h1>
                     <div className='about-statement'>
                         <p>
-                            Want to know what the weather is going to be like today? 
-                            Just open the Weatheria app to get a quick forecast of your local area. 
-                            You can also search weather conditions in cities to see what the weather is like there and get a heads-up before you make plans to travel.
+                        Want to know what the weather is going to be like today? 
+                        Just open the Weatheria app to get quick insights on current weather conditions and forecasts for your local area or other cities located around the world. 
+                        You can also get a head start on the weather by using the app's checklist feature to ensure you start off your morning always well-prepared.
                         </p>
                     </div>
                 </div>
